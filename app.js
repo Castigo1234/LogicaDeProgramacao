54fdcdafbb282966e6c90d5be421e2c498478f27
Castigo1234 <benutob@gmail.com>
2025-12-05 17:37:07 -0300

    Primeiro Commmit no GITterminal

---
+++ app.js
@@ -0,0 +1,16 @@
+/* alert ("Boas vindas ao nosso site!");
+let nome = lua
+let idade = 25
+let numeroDeVendas = 25
+let saldoDisponivel = 1000
+
+alert ("Erro! Preencha todos os campos obrigatórios.");
+let = mensagemDeErro = "Erro! Preencha todos os campos obrigatórios.";
+alert (mensagemDeErro); 
+*/
+
+
+
+
+
+
